ac2d33aef0acab1d6aabfe8162f3bce488bdabd2
Chase Renick <RWolf16@users.noreply.github.com>
2019-07-22 16:16:09 -0400

    0.0.5

--- flaskblog.py
+++ flaskblog.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, url_for, flash, redirect, jsonify, ses
 from forms import SearchForm
 import database_query
 import matching_freeform
+import logit
+import requests
 
-app = Flask(__name__) 
+app = Flask(__name__) #create app variable, set to instance of fla sk class, __name__ = name of module
 
 app.config['SECRET_KEY'] = 'e3295131b9ebfe572bc24bbf74e6137b'
 
@@ -18,18 +20,29 @@ def search():
 
 @app.route("/match", methods = ['GET', 'POST'])
 def match():
-    searchterm = session.get('searchterm', None)
-    choices = matching_freeform.optimizeSearch(searchterm)
+    #Takes the intital search term
+    search_term = session.get('searchterm', None)
+
+    #Logs the search along with the user id (first method)
+    # requests.session
+    # logit.loggingInfo('JohnJacob', search_term)
+
+    #Begins Algorithm of finding exact column name or composite
+    choices = matching_freeform.optimizeSearch(search_term)
+
     return render_template('match.html', choices = choices)
 
 @app.route("/query", methods = ['GET', 'POST'])
 def query():
-    selected_choice = request.args.get('type') #https://stackoverflow.com/questions/50426137/flask-get-clicked-link-info-and-display-on-rendered-page
-    data = database_query.searchDatabase(selected_choice)
+    #Among options presented to the user
+    userSelection = request.args.get('type') #https://stackoverflow.com/questions/50426137/flask-get-clicked-link-info-and-display-on-rendered-page
+
+    #Based on exact search from the user, search dataframe again
+    data = matching_freeform.findRow(userSelection)
 
     original_col_name = data.iloc[0,2]
     revised_col_name = data.iloc[0,3]
-    dataType = data.iloc[0,9]
+    type = data.iloc[0,9]
     length = data.iloc[0,10]
     definition = data.iloc[0, 4]
     tables = data['TABNAME']
@@ -41,7 +54,7 @@ def query():
                             revised_col_name = revised_col_name,
                             definition = definition,
                             tables = unique,
-                            type = dataType,
+                            type = type,
                             length = length)
 
 if __name__ == '__main__':
